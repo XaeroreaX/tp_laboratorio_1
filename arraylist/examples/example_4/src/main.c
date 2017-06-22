f358752a6f88c7e3da3da8d49e0000992d3b515a
XaeroreaX <martinpitameglia@gmail.com>
2017-06-21 22:13:13 -0300

    len

--- arraylist/examples/example_4/src/main.c
+++ arraylist/examples/example_4/src/main.c
@@ -47,10 +47,10 @@ int main(void)
 
     #ifdef TEST
 //        startTesting(1);
-          startTesting(2);
+//          startTesting(2);
 //        startTesting(3);
 //        startTesting(4);
-//        startTesting(5);
+        startTesting(5);
 //        startTesting(6);
 //        startTesting(7);
 //        startTesting(8);
